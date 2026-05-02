91d7901b6dc13555281d74f7212d073302de18ef
atenubi23 <atenubi@gmail.com>
2026-05-02 20:37:18 +0800

    remove unused import for list_view.dart in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'list_view.dart';
 import 'homescreen_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'onboarding_page.dart';
